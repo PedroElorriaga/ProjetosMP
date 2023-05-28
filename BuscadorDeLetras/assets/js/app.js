32e7e620586b7bd1164e58edb1db1fab68769677
Your Name <you@example.com>
2023-05-28 20:49:10 -0300

    Conserto de bug e estilização de resultado

--- BuscadorDeLetras/assets/js/app.js
+++ BuscadorDeLetras/assets/js/app.js
@@ -1,8 +1,8 @@
 const formulario = document.querySelector('.formulario')
 const chaveAPI = 'e4aa2c7fa0dd0824afac8c5eb0ab4be8'
 const resultado = document.querySelector('.resultado')
-const canto = document.querySelector('.cantor')
-const musica = document.querySelector('.musica
+const cantor = document.querySelector('.cantor')
+const musica = document.querySelector('.musica')
 
 
 
@@ -13,10 +13,14 @@ formulario.addEventListener('submit', (e) => {
 
 async function buscarLetra(cantor, musica) {
     await fetch(`https://api.vagalume.com.br/search.php?art=${cantor}&mus=${musica}&apikey=${chaveAPI}`)
-        .then(data => data.json())
-        .then(res => {
-            resultado.innerHTML = res.mus[0].text
-        }).catch(err => {
-            window.alert('Verifique as informações')
-        })
-}
+      .then(data => data.json())
+      .then(res => {
+        const letraFormatada = res.mus[0].text.replace(/\n/g, '<br>'); // Substitui quebras de linha por tags <br>
+        resultado.innerHTML = `<p>${letraFormatada}</p>`; // Envolve cada estrofe em um parágrafo
+      })
+      .catch(error => {
+        window.alert('Verifique as informações');
+      });
+  }
+  
+
