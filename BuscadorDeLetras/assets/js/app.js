d0226e56630c08ee3a7722e26578ecd47f94d8c6
PedroElorriaga <pedrohes2002@outlook.com>
2023-05-28 20:22:30 -0300

    ENVIO DE SCRIPT API

--- BuscadorDeLetras/assets/js/app.js
+++ BuscadorDeLetras/assets/js/app.js
@@ -1,4 +1,22 @@
-console.warn('RODANDO NA FEATURE')
+const formulario = document.querySelector('.formulario')
+const chaveAPI = 'e4aa2c7fa0dd0824afac8c5eb0ab4be8'
+const resultado = document.querySelector('.resultado')
+const canto = document.querySelector('.cantor')
+const musica = document.querySelector('.musica
 
-const conteudo = document.querySelector('.content')
 
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buscarLetra(cantor.value, musica.value)
+})
+
+async function buscarLetra(cantor, musica) {
+    await fetch(`https://api.vagalume.com.br/search.php?art=${cantor}&mus=${musica}&apikey=${chaveAPI}`)
+        .then(data => data.json())
+        .then(res => {
+            resultado.innerHTML = res.mus[0].text
+        }).catch(err => {
+            window.alert('Verifique as informações')
+        })
+}
