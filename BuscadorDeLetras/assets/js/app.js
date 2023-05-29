d849332f94d14af811628e8499d1135d1fdf833d
PedroElorriaga <pedrohes2002@outlook.com>
2023-05-28 21:28:42 -0300

    Tratamento de erros

--- BuscadorDeLetras/assets/js/app.js
+++ BuscadorDeLetras/assets/js/app.js
@@ -5,7 +5,6 @@ const cantor = document.querySelector('.cantor')
 const musica = document.querySelector('.musica')
 
 
-
 formulario.addEventListener('submit', (e) => {
     e.preventDefault()
     buscarLetra(cantor.value, musica.value)
@@ -15,11 +14,14 @@ async function buscarLetra(cantor, musica) {
     await fetch(`https://api.vagalume.com.br/search.php?art=${cantor}&mus=${musica}&apikey=${chaveAPI}`)
       .then(data => data.json())
       .then(res => {
+        if (res.type === 'notfound') resultado.innerHTML = 'Artista não econtrado!'
+        if (res.type === 'song_notfound') resultado.innerHTML = 'Música não encontrada!'
+
         const letraFormatada = res.mus[0].text.replace(/\n/g, '<br>'); // Substitui quebras de linha por tags <br>
         resultado.innerHTML = `<p>${letraFormatada}</p>`; // Envolve cada estrofe em um parágrafo
       })
       .catch(error => {
-        window.alert('Verifique as informações');
+        console.error('ERRO: ' + error)
       });
   }
   
