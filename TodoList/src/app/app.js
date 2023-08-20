57526c7b6fe46758ad7cbb2c44b4983478320179
PedroElorriaga <pedrohes2002@outlook.com>
2023-08-20 19:57:07 -0300

    Inclusão de caminhos estáticos

--- TodoList/src/app/app.js
+++ TodoList/src/app/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const routes = require('../routes/route');
+const path = require('path');
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../../', 'public')));
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, '../', 'views'));
+app.set('view engine', 'html');
 
 app.use(routes);
 
